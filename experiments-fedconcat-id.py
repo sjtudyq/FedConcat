f472c0cab344819b7a6190a06922d07303dbbe2a
sjtudyq <48618508+sjtudyq@users.noreply.github.com>
2023-06-16 14:40:26 +0800

    Update experiments-fedconcat-id.py

--- experiments-fedconcat-id.py
+++ experiments-fedconcat-id.py
@@ -12,6 +12,7 @@ import logging
 import os
 import copy
 from math import *
+from kmeanslimited import *
 from sklearn.cluster import KMeans
 import random
 
@@ -1326,6 +1327,8 @@ if __name__ == '__main__':
         estimator = KMeans(n_clusters=num_K)
         estimator.fit(estimated_dis)
         assign = estimator.labels_
+        if args.dataset in ("cifar100", "tinyimagenet"):
+            assign = constrained_kmeans(estimated_dis,num_K,int(1.2*args.n_parties/args.n_clusters))
         logger.info(assign)
         group = [[] for i in range(num_K)]
         for i in range(args.n_parties):
